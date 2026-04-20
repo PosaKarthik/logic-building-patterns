b65c46e584e29323d44d2d4d23fe3936f7ed962b
Posa Karthik <posakarthik16@gmail.com>
2026-04-20 13:36:26 +0530

    This is a basic square pattern

---
+++ BasicSquarePattern.java
@@ -0,0 +1,18 @@
+public class BasicSquarePattern{
+    public static void main(String[] args){
+        
+        // ****
+        // ****
+        // ****
+        // ****
+
+        int n=4;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("*");   //Here we are using print() instead of println()
+            }
+            System.out.println();
+        }
+    }
+}
