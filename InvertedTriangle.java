d8cd28bb70cd9dca8336409f1c1589d3952b8a03
Posa Karthik <posakarthik16@gmail.com>
2026-04-22 18:44:48 +0530

    Inverted Traingle added

---
+++ InvertedTriangle.java
@@ -0,0 +1,22 @@
+public class InvertedTriangle {
+    public static void main(String[] args){
+
+
+        // * * * *
+        // * * *
+        // * *
+        // *
+
+
+        int n=4;
+
+    for(int i=n-1;i>=0;i--){
+        for(int j=0;j<=i;j++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+
+    }
+    
+}
