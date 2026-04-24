df4e539426e2192e565538c7fdc8cecf48a8e05c
Posa Karthik <posakarthik16@gmail.com>
2026-04-24 16:31:38 +0530

    Pyramid pattern added

---
+++ Pyramid.java
@@ -0,0 +1,23 @@
+public class Pyramid {
+    public static void main(String [] args){
+
+        
+        //    *
+        //   ***
+        //  *****
+        // *******
+
+        int n=4;
+
+        for(int i=0;i<n;i++){
+            for(int j=n-1;j>i;j--){
+                System.out.print(" ");
+            }
+            for(int k=0;k<=2*i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
