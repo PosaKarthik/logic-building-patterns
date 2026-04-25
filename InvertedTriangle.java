f1f5bac1dd506c1e82eb9b108610746e2c7130e7
Posa Karthik <posakarthik16@gmail.com>
2026-04-25 16:29:50 +0530

    InvertedTriangle pattern added

--- InvertedTriangle.java
+++ InvertedTriangle.java
@@ -1,10 +1,19 @@
-public class InvertedTriangle {
-    public static void main(String [] args){
+public class InvertedTriangle{
+    public static void main(String[] args){
+
 
         // * * * *
         // * * *
         // * *
         // *
+
+        int n=4;
+
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
-    
 }
