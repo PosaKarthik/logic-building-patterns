985f81fc4d4be3a10076720c1d1ec531ff8aeab0
Posa Karthik <posakarthik16@gmail.com>
2026-04-25 16:22:47 +0530

    InvertedPyramid pattern added

--- InvertedTriangle.java
+++ InvertedTriangle.java
@@ -1,22 +1,10 @@
 public class InvertedTriangle {
-    public static void main(String[] args){
-
+    public static void main(String [] args){
 
         // * * * *
         // * * *
         // * *
         // *
-
-
-        int n=4;
-
-    for(int i=n-1;i>=0;i--){
-        for(int j=0;j<=i;j++){
-            System.out.print("* ");
-        }
-        System.out.println();
-    }
-
     }
     
 }
