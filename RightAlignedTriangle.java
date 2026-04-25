ace10e426d944a8e23066338ac22c960ca78296f
Posa Karthik <posakarthik16@gmail.com>
2026-04-25 16:45:13 +0530

    RightAlignedTraingle pattern added

---
+++ RightAlignedTriangle.java
@@ -0,0 +1,22 @@
+public class RightAlignedTriangle{
+    public static void main(String [] args){
+
+            //       *
+            //     * *
+            //   * * *
+            // * * * *
+
+            int n=4;
+
+            for(int i=0;i<n;i++){
+                for(int j=i;j<n;j++){
+                    System.out.print("  ");
+                }
+                for(int k=0;k<=i;k++){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+
+    }
+}
